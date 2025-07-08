2c002967c8d2715535c892c8b29fc07de8077e12
jokeradic0710@gmail.com <jokeradic0710@gmail.com>
2025-07-08 18:52:20 +0200

    rename projekat -> server

--- TaksiDispecerskiSistem/TaksiDispecerskiSistem/Server.cs
+++ TaksiDispecerskiSistem/TaksiDispecerskiSistem/Server.cs
@@ -4,12 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TaksiDispecerskiSistem
+namespace Server
 {
-    internal class Program
+    internal class Server
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("hello");
         }
     }
 }
