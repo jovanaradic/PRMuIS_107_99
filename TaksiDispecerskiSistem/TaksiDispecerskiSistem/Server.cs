a91659f940513e3d932c8d2b1a3f8ea4b46623fa
Tatjana <Tatjana@DESKTOP-6Q43N4J>
2025-07-10 13:01:11 +0200

    uradjeno Servis

--- TaksiDispecerskiSistem/TaksiDispecerskiSistem/Server.cs
+++ TaksiDispecerskiSistem/TaksiDispecerskiSistem/Server.cs
@@ -1,16 +1,58 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using System.Threading.Tasks;
+using ZajednickeKlase.Modeli;
 
 namespace Server
 {
-    internal class Server
+    public class Server
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hello");
+
+            //UDP - klijent
+
+            Socket serverSocketUDP = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            //UDP port 500001
+            IPEndPoint serverEPUDP = new IPEndPoint(IPAddress.Any, 50001); // Serverov IPEndPoint, IP i port na kom ce server soket primati poruke
+            serverSocketUDP.Bind(serverEPUDP); // Povezujemo serverov soket sa njegovim EP
+            EndPoint klijentEPUDP = new IPEndPoint(IPAddress.Any, 0); // Serverov IPEndPoint, IP i port na kom ce server soket primati poruke
+
+
+            byte[] bufferKlijent = new byte[1024];
+
+            try
+            {
+                while (true)
+                {
+                    KlijentModel zahtev = null;
+                    int primljenihBajtovaKlijent = serverSocketUDP.ReceiveFrom(bufferKlijent, ref klijentEPUDP);
+                    using (MemoryStream ms = new MemoryStream(bufferKlijent, 0, primljenihBajtovaKlijent))
+                    {
+                        BinaryFormatter bf = new BinaryFormatter();
+                        zahtev = bf.Deserialize(ms) as KlijentModel;
+                        Console.WriteLine("Pristigao klijentski zahtev: ");
+                        Console.WriteLine($"Trenutna pozicija klijenta: {zahtev.pocetnaTacka.X}, {zahtev.pocetnaTacka.Y}.");
+                        Console.WriteLine($"Zeljeno odrediste klijenta: {zahtev.krajnjaTacka.X}, {zahtev.krajnjaTacka.Y}.");
+                    } // kreiranje zadatka nakon zavrsetka voyila
+
+                }
+
+            }
+            catch (SocketException ex)
+            {
+                Console.WriteLine($"Doslo je do greske {ex}");
+            }
+
+            Console.WriteLine("Server zavrsava sa radom");
+            Console.ReadKey();
+            serverSocketUDP.Close();
         }
     }
 }
