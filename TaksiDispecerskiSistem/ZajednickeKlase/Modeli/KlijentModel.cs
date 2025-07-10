7858e32999ec9bfeb61ba5a4dd877fc44782c228
Tatjana <Tatjana@DESKTOP-6Q43N4J>
2025-07-10 09:27:08 +0200

    uradjene klase KlijentModel i Koordinata u folderu Modeli

---
+++ TaksiDispecerskiSistem/ZajednickeKlase/Modeli/KlijentModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZajednickeKlase.Enumeracije;
+
+namespace ZajednickeKlase.Modeli
+{
+    [Serializable]
+    public class KlijentModel
+    {
+        public int IDKlijenta { get; set; }
+        public Koordinata pocetnaTacka { get; set; }
+
+        public Koordinata krajnjaTacka { get; set; }
+
+        public StatusZahteva StatusZahteva { get; set; } = StatusZahteva.Cekanje;
+
+        public KlijentModel() { }
+
+        public KlijentModel(Koordinata pocetnaTacka, Koordinata krajnjaTacka)
+        {
+            this.pocetnaTacka = pocetnaTacka;
+            this.krajnjaTacka = krajnjaTacka;
+        }
+    }
+}
---
+++ TaksiDispecerskiSistem/ZajednickeKlase/Modeli/Koordinata.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZajednickeKlase.Modeli
+{
+    [Serializable]
+    public class Koordinata
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Koordinata() { }
+
+        public Koordinata(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}
