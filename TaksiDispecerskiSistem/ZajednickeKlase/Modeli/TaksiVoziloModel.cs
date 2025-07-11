46ae08b48bd40ac0a5e2539ce38c9fa44ff498e3
jokeradic0710@gmail.com <jokeradic0710@gmail.com>
2025-07-11 12:37:35 +0200

    dodala klasi TaksiVoziloModel

---
+++ TaksiDispecerskiSistem/ZajednickeKlase/Modeli/TaksiVoziloModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZajednickeKlase.Enumeracije;
+
+namespace ZajednickeKlase.Modeli
+{
+    [Serializable]
+    public class TaksiVoziloModel
+    {
+        public int koordinataX { get; set; }
+        public int koordinataY { get; set; }
+        public StatusVozila Status { get; set; } = StatusVozila.Slobodno;
+        public double Km { get; set; }
+        public double Zarada { get; set; }
+
+        public TaksiVoziloModel() { }
+
+        public TaksiVoziloModel(int koordinataX, int koordinataY)
+        {
+            this.koordinataX = koordinataX;
+            this.koordinataY = koordinataY;
+        }
+    }
+}
