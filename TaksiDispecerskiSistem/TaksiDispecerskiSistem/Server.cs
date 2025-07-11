f412ec60d57fabd992e5bbd02aabddc33da64cfa
jokeradic0710@gmail.com <jokeradic0710@gmail.com>
2025-07-11 15:57:53 +0200

    komunikacija TaksiVozilo - Server

--- TaksiDispecerskiSistem/TaksiDispecerskiSistem/Server.cs
+++ TaksiDispecerskiSistem/TaksiDispecerskiSistem/Server.cs
@@ -8,6 +8,7 @@ using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using System.Threading.Tasks;
 using ZajednickeKlase.Modeli;
+using ZajednickeKlase.Enumeracije;
 
 namespace Server
 {
@@ -15,6 +16,19 @@ namespace Server
     {
         static void Main(string[] args)
         {
+            //TCP - vozilo
+
+            Socket serverSocketTCP = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            //TCP port 50000
+            IPEndPoint serverEPTCP = new IPEndPoint(IPAddress.Any, 50000);
+            serverSocketTCP.Bind(serverEPTCP);
+            int maxKlijenata = 1;
+            serverSocketTCP.Listen(maxKlijenata);
+            Socket voziloSocketTCP = serverSocketTCP.Accept();
+            IPEndPoint voziloEPTCP = voziloSocketTCP.RemoteEndPoint as IPEndPoint;
+
+            Console.WriteLine($"Povezalo se vozilo, njegova adresa je {voziloEPTCP}");
 
             //UDP - klijent
 
@@ -26,11 +40,34 @@ namespace Server
 
 
             byte[] bufferKlijent = new byte[1024];
+            byte[] bufferVozilo = new byte[1024];
 
             try
             {
                 while (true)
                 {
+                    int primljeniBajtoviVozilo = voziloSocketTCP.Receive(bufferVozilo);
+                    TaksiVoziloModel taksiVozilo = null;
+                    using (MemoryStream ms = new MemoryStream(bufferVozilo, 0, primljeniBajtoviVozilo))
+                    {
+                        BinaryFormatter bf = new BinaryFormatter();
+                        taksiVozilo = bf.Deserialize(ms) as TaksiVoziloModel;
+                        Console.WriteLine($"Trenutna pozicija vozila: {taksiVozilo.koordinataX}, {taksiVozilo.koordinataY}");
+                        if (taksiVozilo.Status == StatusVozila.NaPutu)
+                        {
+                            Console.WriteLine($"Status vozila: na putu");
+                        }
+                        else if (taksiVozilo.Status == StatusVozila.UVoznji)
+                        {
+                            Console.WriteLine($"Status vozila: u voznji");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Status vozila: cekanje");
+                        }
+
+                    }
+
                     KlijentModel zahtev = null;
                     int primljenihBajtovaKlijent = serverSocketUDP.ReceiveFrom(bufferKlijent, ref klijentEPUDP);
                     using (MemoryStream ms = new MemoryStream(bufferKlijent, 0, primljenihBajtovaKlijent))
@@ -40,7 +77,25 @@ namespace Server
                         Console.WriteLine("Pristigao klijentski zahtev: ");
                         Console.WriteLine($"Trenutna pozicija klijenta: {zahtev.pocetnaTacka.X}, {zahtev.pocetnaTacka.Y}.");
                         Console.WriteLine($"Zeljeno odrediste klijenta: {zahtev.krajnjaTacka.X}, {zahtev.krajnjaTacka.Y}.");
-                    } // kreiranje zadatka nakon zavrsetka voyila
+                    }
+
+                    ZadatakModel zadatak = new ZadatakModel
+                    {
+                        IDKlijenta = zahtev.IDKlijenta,
+                        //unijeti id u model vozila
+                        IDVozila = 2,
+                        PredjenaRazdaljina = 3
+                    };
+                    byte[] bufferZadatak = new byte[1024];
+
+                    //slanje zahteva serveru
+                    using (MemoryStream ms = new MemoryStream())
+                    {
+                        BinaryFormatter bf = new BinaryFormatter();
+                        bf.Serialize(ms, zadatak);
+                        bufferZadatak = ms.ToArray();
+                        int brBajta = voziloSocketTCP.SendTo(bufferZadatak, 0, bufferZadatak.Length, SocketFlags.None, voziloEPTCP);
+                    }
 
                 }
 
@@ -53,6 +108,8 @@ namespace Server
             Console.WriteLine("Server zavrsava sa radom");
             Console.ReadKey();
             serverSocketUDP.Close();
+            serverSocketTCP.Close();
+            voziloSocketTCP.Close();
         }
     }
 }
--- TaksiDispecerskiSistem/TaksiVozilo/TaksiVozilo.cs
+++ TaksiDispecerskiSistem/TaksiVozilo/TaksiVozilo.cs
@@ -1,8 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using System.Threading.Tasks;
+using ZajednickeKlase.Modeli;
+using ZajednickeKlase.Enumeracije;
 
 namespace TaksiVozilo
 {
@@ -10,6 +16,100 @@ namespace TaksiVozilo
     {
         static void Main(string[] args)
         {
+            Console.Title = "VOZILO";
+            Console.Write("ID vozila: ");
+            int id = int.Parse(Console.ReadLine() ?? "0");
+
+            Random r = new Random();
+            Koordinata lokacija = new Koordinata(r.Next(0, 20), r.Next(0, 20));
+
+            Socket clientSocketTCP = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint serverEP = new IPEndPoint(IPAddress.Loopback, 50000);
+            clientSocketTCP.Connect(serverEP);
+
+            TaksiVoziloModel vozilo = new TaksiVoziloModel
+            {
+                koordinataX = lokacija.X,
+                koordinataY = lokacija.Y,
+                Status = StatusVozila.Slobodno
+            };
+
+            byte[] bufferStatusVoznje = new byte[1024];
+
+            //slanje statusa voznje serveru
+            using (MemoryStream ms = new MemoryStream())
+            {
+                BinaryFormatter bf = new BinaryFormatter();
+                bf.Serialize(ms, vozilo);
+                bufferStatusVoznje = ms.ToArray();
+                int brBajta = clientSocketTCP.SendTo(bufferStatusVoznje, 0, bufferStatusVoznje.Length, SocketFlags.None, serverEP);
+            }
+
+            Console.Clear();
+            Console.WriteLine($"VOZILO {id} povezano i spremno.\n");
+
+            while (true)
+            {
+                try
+                {
+                    byte[] bufferZadatak = new byte[1024];
+                    int velicinaZadatka = clientSocketTCP.Receive(bufferZadatak);
+
+                    ZadatakModel zadatak = null;
+
+                    using (MemoryStream ms = new MemoryStream(bufferZadatak, 0, velicinaZadatka))
+                    {
+                        BinaryFormatter bf = new BinaryFormatter();
+                        zadatak = bf.Deserialize(ms) as ZadatakModel;
+
+                    }
+                    if (zadatak != null)
+                    {
+
+                        Console.WriteLine($"Nova voznja → klijent {zadatak.IDKlijenta}, {zadatak.PredjenaRazdaljina:F1} km");
+                        vozilo.Status = StatusVozila.NaPutu;
+
+                        // SIMULACIJA KRETANJA -> u simulaciji saljem vozilo
+
+                        vozilo.Status = StatusVozila.UVoznji;
+
+                        // SIMULACIJA KRETANJA
+
+
+                        StatusVoznje status = new StatusVoznje
+                        {
+                            IdKlijenta = zadatak.IDKlijenta,
+                            IdVozila = id,
+                            Km = zadatak.PredjenaRazdaljina,
+                            CenaVoznje = zadatak.PredjenaRazdaljina * 0.8
+                        };
+
+
+                        //saljem status StatusVoznje -> ridefinisher
+
+                        vozilo.Status = StatusVozila.Slobodno;
+
+                        //saljem vozilo
+                    }
+                    else
+                    {
+                        Console.WriteLine("Veza sa serverom prekinuta!");
+                        break;
+                    }
+
+
+                }
+
+                catch (SocketException ex)
+                {
+                    Console.WriteLine($"Doslo je do greske tokom slanja:\n{ex}");
+                }
+            }
+
+            Console.WriteLine("Klijent zavrsava sa radom");
+            Console.ReadKey();
+            clientSocketTCP.Close();
+
         }
     }
 }
