95e238f6443c2b18e9611d54e8194b63ff7e4db5
jokeradic0710@gmail.com <jokeradic0710@gmail.com>
2025-07-11 12:45:54 +0200

    ZadatakModel -> dodala

---
+++ TaksiDispecerskiSistem/ZajednickeKlase/Modeli/ZadatakModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZajednickeKlase.Enumeracije;
+
+namespace ZajednickeKlase.Modeli
+{
+    [Serializable]
+
+    public class ZadatakModel
+    {
+        public int IDKlijenta { get; set; }
+        public int IDVozila { get; set; }
+
+        public StatusZadatka StatusVozila { get; set; } = StatusZadatka.Aktivan;
+
+        public double PredjenaRazdaljina { get; set; }
+
+        public ZadatakModel(int iDKlijenta, int iDVozila, StatusZadatka statusVozila, double predjenaRazdaljina)
+        {
+            IDKlijenta = iDKlijenta;
+            IDVozila = iDVozila;
+            StatusVozila = statusVozila;
+            PredjenaRazdaljina = predjenaRazdaljina;
+        }
+
+        public ZadatakModel() { }
+    }
+}
