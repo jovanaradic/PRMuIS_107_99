aa9c183c3210826a050ea0b4ac5d5af846364766
jokeradic0710@gmail.com <jokeradic0710@gmail.com>
2025-07-11 12:39:44 +0200

    dodala klasu StatusVoznje model

---
+++ TaksiDispecerskiSistem/ZajednickeKlase/Modeli/StatusVoznje.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZajednickeKlase.Modeli
+{
+    [Serializable]
+    public class StatusVoznje
+    {
+
+        public int IdKlijenta { get; set; }
+        public int IdVozila { get; set; }
+        public double Km { get; set; }
+        public double CenaVoznje { get; set; }
+
+        public StatusVoznje() { }
+    }
+}
